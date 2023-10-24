62465880fec78b1bff14dd655907005e045e30d1
lerpaiva <leticiarpaival@gmail.com>
2023-10-24 07:36:37 -0300

    uppp

---
+++ script.js
@@ -0,0 +1,33 @@
+const tabela = document.querySelector('.tabela-js')
+
+
+
+
+axios.get('https://apiaulas--leticiarpaival.repl.co/funcionarios').then(function(resposta){
+    getData(resposta.data)
+}).catch(function(error){
+    console.log(error)
+})
+
+function getData(dados){
+    dados.map((item)=>{
+        tabela.innerHTML += `
+        <tr>
+            <th scope="row">${item.id}</th>
+            <td>${item.nome}</td>
+            <td>${item.cargo}</td>
+            <td>${item.salario}</td>
+            <td><span class="material-symbols-outlined text-danger">
+            delete
+            </span>
+            <span class="material-symbols-outlined text-success">
+            edit
+            </span>
+            <td>
+        </tr>
+        `
+        })
+
+    }
+
+
